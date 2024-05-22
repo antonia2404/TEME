2d76cd5a5f9d13b4867563e3e17adb1c990a80c7
antonia2404 <147429405+antonia2404@users.noreply.github.com>
2024-05-22 13:34:56 +0300

    Delete lab6-tema directory

--- lab6-tema/lab6-1/src/Animal.java
+++
@@ -1,9 +0,0 @@
-public class Animal {
-    void eat() {
-        System.out.println("This animal eats something.");
-    }
-
-    void sound() {
-        System.out.println("This animal makes a sound.");
-    }
-}
